80061b96cee279d9305088da88e8bc5e0efc2902
Ogekay <ogeeucharia9@gmail.com>
2022-12-13 03:17:10 -0800

    Good

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
+#!/bin/bash
 #include <stdio.h>
 /**
 * main -main block
-*return(0)
+* Return:0
 */
-    int main(void)
+     int main(void)
 {
-    puts("\"programming is like building a miultilingual puzzle");
-     return(0)
+      puts("\"Programming is like building a multilingual puzzle");
+    return (0)
+}
